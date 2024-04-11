10c9c4dc6e3f07a19149aeb50c2f4966f891e220
root <hakeemabdullah87@gmail.com>
2024-04-11 03:09:49 -0700

    FEAT: add a py script file 0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/user/bin/python3
+"""Writing a script to determine pascal's triangle for numbers"""
+
+
+def pascal_triangle(n):
+    """
+    returns a list of integers representing the pascals' triangle of n
+    """
+    triangle = []
+
+    if n <= 0:
+        return triangle
+
+    for i in range(n):
+        temp_list = []
+
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(temp_list)
+
+    return triangle
