8b217f2b2eb54042d7fcedbc41ad99847539a577
root <hakeemabdullah87@gmail.com>
2024-04-25 17:28:38 -0700

    FEAT: add the script py file 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""In a text file, theres is a single character H. Text editor can execute only
+two operations in this file: Copy All and paste. Given a number n write a method
+that calculate the fewest number operations need to result in exactly n.
+"""
+
+
+def minOperations(n):
+    """A function that calcuate fewest no of operation needed in n"""
+    total = 0
+    operations = 2
+
+    while n > 1:
+        while not n % operations:
+            total += operations
+            n /= operations
+        operations += 1
+    return total
