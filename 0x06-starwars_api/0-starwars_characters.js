67e6b336907c33ff5a392a69ed5a36d025f0fab4
Abdullahi <hakeemabdullah87@gmail.com>
2024-05-23 21:25:00 +0000

    BUGFIX: fix semistandard error

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,26 +7,25 @@ const movieId = process.argv[2];
 const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films' + movieId;
 
 function sendRequest (characterList, index) {
-    if (characterList.length == index) {
-	return;
-    }
+  if (characterList.length === index) {
+    return;
+  }
 
-    request(characterList[index], (error, response, body) => {
-	if (error) {
-	    console.log(error);
-	} else {
-	    console.log(JSON.parse(body).name);
-	    sendRequest(characterList, index + 1);
-	}
-    });
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
+    }
+  });
 }
 
 request(movieEndpoint, (error, response, body) => {
-    if (error) {
-	console.log(error);
-    } else {
-	const characterList = JSON.parse(body).characters;
-	sendRequest(characterList, 0);
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
+    sendRequest(characterList, 0);
+  }
 });
-	
