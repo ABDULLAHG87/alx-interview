9b4a4f28bfe53e90a800865a11a82e67f91421af
Abdullahi <hakeemabdullah87@gmail.com>
2024-05-23 20:52:15 +0000

    BUGFIX: fix error with the code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,7 +4,7 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const movieEndpoint = https://swapi-api.alx-tools.com/api/films' +movieId;
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films' + movieId;
 
 function sendRequest (characterList, index) {
     if (characterList.length == index) {
@@ -25,7 +25,7 @@ request(movieEndpoint, (error, response, body) => {
     if (error) {
 	console.log(error);
     } else {
-	constant characterList = JSON.parse(body).characters;
+	const characterList = JSON.parse(body).characters;
 	sendRequest(characterList, 0);
     }
 });
